0758a97d8c283a8ae22ffc382e24ec66d72a43bf
Michael Champagne <mch1307@users.noreply.github.com>
2017-08-17 07:26:04 +0200

    GetKV to read value from Consul k/v

--- svc/consul.go
+++ svc/consul.go
@@ -7,8 +7,8 @@ import (
 	api "github.com/hashicorp/consul/api"
 )
 
-//NewClient returns a consul client instance
-func NewClient(consulAddr, consulPort, consulToken string) (Consul *api.Client) {
+// NewClient returns a consul client instance
+func NewClient(consulAddr, consulPort, consulToken string) (Consul *api.Client, err error) {
 	config := api.DefaultConfig()
 	config.Address = consulAddr + ":" + consulPort
 	config.Token = consulToken
@@ -16,7 +16,7 @@ func NewClient(consulAddr, consulPort, consulToken string) (Consul *api.Client) 
 	if err != nil {
 		fmt.Println("error connecting to consul ", err)
 	}
-	return consul
+	return consul, err
 }
 
 // RegisterService register the service to Consul service catalog
@@ -34,7 +34,7 @@ func RegisterService(ag *api.Agent, service, hostname, protocol string, port int
 			Interval: "10s",
 			Timeout:  "8s",
 			TTL:      "",
-			HTTP:     protocol + "://" + hostname + ":" + strconv.Itoa(port) + "/health",
+			HTTP:     protocol + "://" + "172.30.16.47" + ":" + strconv.Itoa(port) + "/health",
 			Status:   "passing",
 		},
 		Checks: api.AgentServiceChecks{},
@@ -51,4 +51,21 @@ func RegisterService(ag *api.Agent, service, hostname, protocol string, port int
 func DeregisterService(ag *api.Agent, service string) {
 	_ = ag.ServiceDeregister(service)
 
+}
+
+// GetKV get value of key from Consul K/V store and returns as string
+func GetKV(cli *api.Client, key string) (string, error) {
+	var ret string
+	kv := cli.KV()
+	val, _, err := kv.Get(key, nil)
+	if err != nil {
+		fmt.Println(err)
+		val.Value = nil
+	}
+	if val != nil {
+		ret = string(val.Value)
+	} else {
+		ret = "not_found"
+	}
+	return ret, err
 }
